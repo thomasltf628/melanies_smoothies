7e587fe74585f363535a93d5d6ebcf92f9286d98
thomasltf628 <49809873+thomasltf628@users.noreply.github.com>
2025-11-14 15:14:42 -0500

    Uncomment dataframe display and stop execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
     ingredients_string = ''
@@ -32,7 +33,6 @@ if ingredients_list:
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
     st.write(my_insert_stmt)
-    #st.stop()
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
